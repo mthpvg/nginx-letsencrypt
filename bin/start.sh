5989e674102b29cc2648db5bced2f4e7f076b750
mthpvg <mth.pvg@gmail.com>
2017-07-16 16:21:56 +0200

    fix: multiple

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-./stop.sh
+./bin/stop.sh
 
+cd ..
+
+echo "Execute: docker build"
 docker build -t static-nginx .
 
+echo "Execute: docker build"
 docker run \
-  -d \
   -v $(pwd)/client:/wwww/data:ro -p 80:80 \
   --name=static-nginx \
   static-nginx
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Execute: docker stop"
 docker stop static-nginx
+echo "Execute: docker remove"
 docker rm static-nginx
