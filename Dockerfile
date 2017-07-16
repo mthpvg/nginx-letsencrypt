ddca74a7d68506dbcdd3ba7ea1efc1184bb30c30
mthpvg <mth.pvg@gmail.com>
2017-07-16 16:29:40 +0200

    chore: COPY

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM nginx:1.12.1
 
 WORKDIR /www/data
+COPY ./client /wwww/data
 RUN rm /etc/nginx/conf.d/default.conf
 COPY ./nginx/*.conf /etc/nginx/conf.d/
 
--- bin/start.sh
+++ bin/start.sh
@@ -2,13 +2,12 @@
 
 ./bin/stop.sh
 
-cd ..
-
 echo "Execute: docker build"
 docker build -t static-nginx .
 
 echo "Execute: docker build"
 docker run \
-  -v $(pwd)/client:/wwww/data:ro -p 80:80 \
+  -d \
+  -p 80:80 \
   --name=static-nginx \
   static-nginx
