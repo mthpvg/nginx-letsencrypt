dae6a4f42fa6acf2771f2bd73afcaaae07b3f5cc
Mathieu Pavageau <mth.pvg@gmail.com>
2017-07-25 19:18:24 +0200

    feat: auto renewal

--- bin/create_website.sh
+++ bin/create_website.sh
@@ -5,20 +5,20 @@
 
 set -eu
 
+echo "Creating a directory for $2"
 cd ./client
-
 mkdir $2
 cp index.template.html $2/index.html
 
+echo "Setting a dummy Nginx config for Letsencrypt ACME challenge"
 cd ../nginx
+sed -e "s/SERVERNAME/$2/g" -e "s/WEBSITE_DIRECTORY/$2/g" template > $2.conf
 
-sed -e "s/SERVERNAME/$2/g" -e "s/WEBSITE_DIRECTORY/$2/g" ssl-template > $2.conf
-
+echo "Restarting Nginx"
 docker restart static-nginx
 
+echo "Starting Letsencrypt ACME challenge"
 cd ../client
-
-DOMAINS='-d $2'
 
 sudo certbot certonly \
     --webroot \
@@ -26,4 +26,17 @@ sudo certbot certonly \
     --agree-tos \
     --no-eff-email \
     --email $1 \
-    $DOMAINS
+    -d $2
+
+echo "Setting the real Nginx config"
+cd ../nginx
+sed -e "s/SERVERNAME/$2/g" -e "s/WEBSITE_DIRECTORY/$2/g" ssl-template > $2.conf
+
+echo "Restarting Nginx"
+docker restart static-nginx
+
+echo "Checking auto-renewal"
+/usr/bin/certbot renew --dry-run
+
+echo "Enabling auto renewal"
+crontab -l | grep -q 'certbot'  && echo 'Auto Renewal is already in place' || (crontab -l 2>/dev/null; echo '15 3 * * * /usr/bin/certbot renew --quiet --renew-hook "docker restart static-nginx') | crontab -
