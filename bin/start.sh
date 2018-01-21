e5742fdf5da68e06b1471757465409ebba5417b4
Mathieu Pavageau <mth.pvg@gmail.com>
2018-01-21 14:03:56 +0100

    feat: add memory monitoring

--- bin/start.sh
+++ bin/start.sh
@@ -26,3 +26,8 @@ echo 'Generating a SSH key'
 ssh-keygen -t rsa
 echo "Copy the following public key in the Github's UI"
 cat ~/.ssh/id_rsa.pub
+
+echo 'Add memory monitoring'
+sudo apt-get install -y bc
+git clone https://github.com/mthpvg/bash-monitoring-memory.git
+echo '~/bash-monitoring-memory/stats.sh' >> ~/.bashrc
